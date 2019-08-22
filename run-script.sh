1eb5969ff3bb818be7fbf45449e807e135115c42
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2019-08-22 07:51:57 +0200

    Update run-script.sh

--- run-script.sh
+++ run-script.sh
@@ -1,6 +1,6 @@
 sh odoo-install.sh
 rm -rf /etc/nginx/sites-available/default
-cp dfault /etc/nginx/sites-available/
+cp default /etc/nginx/sites-available/
 rm -rf /etc/odoo-server.conf
 cp odoo-server.conf /etc/
 chown odoo:odoo /etc/odoo-server.conf
