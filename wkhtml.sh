56a8895b314e2d97eae7bdce2908232f8426d197
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2019-08-29 01:09:49 +0200

    Update and rename whtml1.sh to wkhtml.sh

---
+++ wkhtml.sh
@@ -0,0 +1,5 @@
+sudo wget https://builds.wkhtmltopdf.org/0.12.1.3/wkhtmltox_0.12.1.3-1~bionic_amd64.deb
+sudo dpkg -i wkhtmltox_0.12.1.3-1~bionic_amd64.deb
+sudo apt-get install -f
+sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin
+sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin
